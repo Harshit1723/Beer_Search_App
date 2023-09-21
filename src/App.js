a140efd953d94e93a3438254ca4fdff2195bd2d5
Harshit <harshitsingh172023@gmail.com>
2023-09-21 12:28:32 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [beers,setBeers] = useState([]);
+  const[searchTerm,setSearchTerm] =useState("");
+
+  useEffect(() => {
+    const Data = async() => {
+      try{
+        const reuslt =  await fetch("https://api.punkapi.com/v2/beers");
+        const data = await reuslt.json();
+
+        setBeers(data);
+      }catch(error) {
+        console.log(error);
+      }
+    }
+
+    Data();
+  },[]);
+
+  const SearchedBeer = beers.filter((beer) => beer.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  
+
+  return(
+    <div>
+
+    <div className='searching'>
+    <input type="text" placeholder='Search beers...' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+   
+    </div>
+
+     <div className='beer'>
+      {SearchedBeer.map((beer) => (
+
+        
+
+<div key={beer.id} className='beer-item'>
+           <h3 className="text-xl font-semibold">{beer.name}</h3>
+           
+          <img  src={beer.image_url} alt={beer.name} className='beer-image'/>
+          <p className="text-gray-600">{beer.tagline}</p>
+          </div>
+
+
+        
+
+        
+      ))}
+      </div>
     </div>
   );
 }
